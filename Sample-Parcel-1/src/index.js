2c7095f9e96e33d3da23e4042632e205cb057aad
Alexis Miguel Jimenez Olivares <alexis.m.jimenez.o@gmail.com>
2019-03-03 08:42:21 -0600

    :sparkles: Added computed properties.

--- Sample-Parcel-1/src/index.js
+++ Sample-Parcel-1/src/index.js
@@ -11,15 +11,29 @@ new Vue({
     finishedLink: '<a href="http://www.google.com">Google</a>',
     counter: 0,
     x: 0,
-    y: 0
+    y: 0,
+    otherCounter: 0
+  },
+  computed: {
+    output: function() {
+      console.log('computed')
+      return this.otherCounter > 5 ? 'Greater than 5' : 'Smaller than 5'
+    }
   },
   methods: {
     increase: function(step, event) {
       this.counter += step
     },
+    result: function() {
+      console.log('method')
+      return this.otherCounter > 5 ? 'Greater than 5' : 'Smaller than 5'
+    },
     updateCoordinates: function(event) {
       this.x = event.clientX
       this.y = event.clientY
+    },
+    alertMe: function() {
+      alert('Alert!')
     }
   }
 })
